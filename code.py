28fa4f8f26d5814ba2e7f62cad352709b54fb07c
chaitanyapentakota <117889524+chaitanyapentakota@users.noreply.github.com>
2022-11-10 15:35:28 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,11 +1,9 @@
-def get_value(val):
-    for key, value in people.items():
-        if val == key:
-            print(value)
+def people()
 
-people = {1: {'name': 'John', 'age': '27', 'sex': 'Male','EmpId':'9091'},
-          2: {'name': 'Marie', 'age': '25', 'sex': 'Female','EmpId':'9092'},
-          3: {'name': 'chaitanya', 'age':'23','sex': 'male','EmpId':'9093'},
-          4: {'name': 'lahari', 'age': '22', 'sex': 'Female','EmpId':'9094'}}
-          
-get_value(1)
+    people = {1: {'name': 'John', 'age': '27', 'sex': 'Male','EmpId':'9091'},
+              2: {'name': 'Marie', 'age': '25', 'sex': 'Female','EmpId':'9092'},
+              3: {'name': 'chaitanya', 'age':'23','sex': 'male','EmpId':'9093'},
+              4: {'name': 'lahari', 'age': '22', 'sex': 'Female','EmpId':'9094'}}
+
+     print(people)          
+people()
